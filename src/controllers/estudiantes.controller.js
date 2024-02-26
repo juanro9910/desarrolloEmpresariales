7bf64d08f809b1793bdc34538ee8923526b6fdf1
juanro9910 <juanro9910@gmail.com>
2024-02-26 11:59:30 -0500

    Ajuste consulta por id

--- src/controllers/estudiantes.controller.js
+++ src/controllers/estudiantes.controller.js
@@ -1,10 +1,39 @@
 import db from "../db/mysql.js";
 
-
-export const consultarEstudiantes = async (req,res) => {
+// consulta todos los estudiante
+export const consultarEstudiantes = async (req, res) => {
     try {
         // console.log('Se ejecuto el controlador de consultar estudiante');
         db.query('SELECT * FROM estudiantes', (error, results, fields) => {
+            if (error) {
+                // console.error('Error en la consulta: ', error);
+                return res.status(500).json({
+                    message: 'Error en la consulta',
+                    error: error
+                });
+            } else {
+                // console.log('Registros obtenidos:', results);
+                return res.status(200).json({
+                    message: 'Consulta exitosa',
+                    results: results
+                });
+            }
+        });
+    } catch (error) {
+        // console.error('Error en el bloque catch: ', error);
+        return res.status(400).json({
+            message: 'Consulta no exitosa',
+            error: error
+        });
+    }
+}
+
+// consulta por id estudiante
+export const consultarEstudianteById = async (req, res) => {
+    const estudianteId = req.params.id;
+    try {
+        // console.log('Se ejecuto el controlador de consultar estudiante');
+        db.query('SELECT * FROM estudiantes WHERE idestuadiantes = ?', [estudianteId], (error, results, fields) => {
             if (error) {
                 // console.error('Error en la consulta: ', error);
                 return res.status(500).json({
--- src/routes/estudiantes.routes.js
+++ src/routes/estudiantes.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { consultarEstudiantes } from "../controllers/estudiantes.controller.js";
+import { consultarEstudianteById } from "../controllers/estudiantes.controller.js";
 
 const router = Router();
 
 router.get('/estudiantes', consultarEstudiantes);
+
+router.get('/estudiante/:id', consultarEstudianteById);
 
 export default router;
